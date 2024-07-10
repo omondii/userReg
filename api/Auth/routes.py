9cd6ea2fe4224480d8eac02df47024d7684311f8
Brian Omondi <omondiibrian00@gmail.com>
2024-07-11 00:30:07 +0300

    Almost deployed

--- api/Auth/routes.py
+++ api/Auth/routes.py
@@ -6,7 +6,7 @@ from flask import request, jsonify
 from Auth import auth
 from Models import storage
 # from api.Auth.forms import RegistrationForm
-from tables import User, Organisation
+from Models.tables import User, Organisation
 from flask_jwt_extended import (create_access_token,
                                 unset_jwt_cookies, get_jwt, get_jwt_identity, jwt_required)
 from werkzeug.security import generate_password_hash, check_password_hash
